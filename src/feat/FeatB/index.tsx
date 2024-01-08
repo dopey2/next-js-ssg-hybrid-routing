9a63dbdf5994530ae6658b10d6e30e30e6e874ce
unknown <dani.gabor1996@gmail.com>
2024-01-08 20:26:18 +0100

    feat(layout): set images inside a fixed size div for both featA & featB

--- src/feat/FeatB/index.tsx
+++ src/feat/FeatB/index.tsx
@@ -8,13 +8,15 @@ const Index = () => {
         <div className={"flex flex-1 flex-col items-center justify-center"}>
             <h1 className={"text-2xl"}>Feat B</h1>
             <h2 className={"text-xl"}>{i18n.t("app.featB.text")}</h2>
-            <Image
-                src={img}
-                alt={"cats"}
-                className={"rounded-2xl mt-8"}
-                width={500}
-                height={300}
-            />
+            <div style={{ width: "500px", height: "300px" }}>
+                <Image
+                    src={img}
+                    alt={"cats"}
+                    className={"rounded-2xl mt-8"}
+                    width={500}
+                    height={300}
+                />
+            </div>
         </div>
 
     );
