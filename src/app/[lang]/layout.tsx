1fd133e911c3d5ef847061f9952611ab33e880fe
unknown <dani.gabor1996@gmail.com>
2024-01-08 20:48:03 +0100

    feat(layout): generate hreflang alternative url

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -18,10 +18,32 @@ export async function generateStaticParams(): Promise<Array<Params>> {
     ];
 }
 
+const getAlternateHrefUrls = (lang: string) => {
+    const hrefUrls = [
+        {
+            lang: "fr",
+            hrefLang: "fr-FR",
+            href: "https://www.localhost:3000/fr",
+        },
+        {
+            lang: "en",
+            hrefLang: "en-US",
+            href: "https://www.localhost:3000/en",
+        }
+    ];
+
+    return hrefUrls.filter((l) => l.lang !== lang);
+};
+
 export default function RootLayout(props: Props) {
+    const alternateHrefUrl = getAlternateHrefUrls(props.params.lang);
+
     return (
         <html lang={props.params.lang}>
-            <link rel="alternate" hrefLang="en-US" href="https://www.localhost:3000/en" />
+            {alternateHrefUrl.map((data) => (
+                <link key={data.hrefLang} rel="alternate" hrefLang={data.hrefLang} href={data.href} />
+            ))}
+
             <body>
                 <div id="root" className={"w-full h-full flex flex-1 flex-col"}>
                     {props.children}
